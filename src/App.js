2f6ad596b9f2525e1a1492ae9697bd57fd78b976
Suzuki Moeto <suzukimoeto@suzukinoMacBook-Pro.local>
2021-01-17 02:13:35 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
-function App() {
-    return (
-	    <div>
-	    <h1>Hello World</h1>
-	    </div>
-  );
+
+// class App extends Component {
+// 	render() {
+// 		return (
+// 			<React.Fragment>
+// 				<h1>よろしくお願いします。</h1>
+// 			</React.Fragment>
+// 		)
+
+// 	}
+// }
+
+// function App() {
+//     return (
+// 	    <div>
+// 	    <h1>Hello World</h1>
+// 	    </div>
+//   );
+// }
+
+const App = () => {
+	return <div>Hi!</div>
 }
 
 export default App;
